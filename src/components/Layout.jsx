d3ff9b1517e51120957e65263454a9d75485c528
JmprXDrk <piskar2008@yandex.ru>
2023-01-27 14:53:22 +0300

    email form

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -15,7 +15,11 @@ export default function Layout({ initState }) {
           integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD"
           crossOrigin="anonymous"
         />
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossOrigin="anonymous" />
+        <script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
+          crossOrigin="anonymous"
+        />
         <script
           type="text/javascript"
           dangerouslySetInnerHTML={{
@@ -25,7 +29,7 @@ export default function Layout({ initState }) {
         <link rel="stylesheet" href="style/null.css" />
         <script defer src="/app.js" />
         <script defer src="/vendor.js" />
-        <title>Document</title>
+        <title>Social Pharmacy</title>
       </head>
       <body>
         <div id="root">
